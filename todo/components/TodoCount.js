aa3e70095057658ac66d4c24f4fef8754ffb1845
Yannick Bochatay <yannick.bochatay@free.fr>
2026-01-04 21:07:25 +0100

    réorg des méthodes de TodoCount

--- todo/components/TodoCount.js
+++ todo/components/TodoCount.js
@@ -5,16 +5,16 @@ class TodoCount extends HTMLSpanElement {
 	get #count() {
 	  return tasks.reduce((sum, item) => item.completed ? sum : sum+1, 0);
 	}
-
-	#handleStateChange = prop => {
-		if (prop === "completed" || prop === "length") this.#update();
-	}
 	
 	#update() {
 		let count = this.#count;
 		this.innerHTML = `<strong>${count}</strong> item${count === 1 ? "" : "s"} left`;
 	}
-	
+
+	#handleStateChange = prop => {
+		if (prop === "completed" || prop === "length") this.#update();
+	}
+		
 	connectedCallback() {
 	  this.#update();
 	  onStateChange(this.#handleStateChange);
